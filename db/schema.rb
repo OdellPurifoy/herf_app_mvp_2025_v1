c59363e77eeb54d45a11d7caa826af79fa48f16a
Odell Purifoy <opurifoy@springhealth.com>
2025-01-10 21:36:43 -0500

    lint: Ran rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -10,80 +10,81 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_11_022252) do
+ActiveRecord::Schema[7.1].define(version: 20_250_111_022_252) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "active_storage_attachments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.uuid "record_id", null: false
-    t.uuid "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  create_table 'active_storage_attachments', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
+    t.string 'name', null: false
+    t.string 'record_type', null: false
+    t.uuid 'record_id', null: false
+    t.uuid 'blob_id', null: false
+    t.datetime 'created_at', null: false
+    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
+    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness',
+                                                    unique: true
   end
 
-  create_table "active_storage_blobs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  create_table 'active_storage_blobs', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
+    t.string 'key', null: false
+    t.string 'filename', null: false
+    t.string 'content_type'
+    t.text 'metadata'
+    t.string 'service_name', null: false
+    t.bigint 'byte_size', null: false
+    t.string 'checksum'
+    t.datetime 'created_at', null: false
+    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
   end
 
-  create_table "active_storage_variant_records", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  create_table 'active_storage_variant_records', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
+    t.uuid 'blob_id', null: false
+    t.string 'variation_digest', null: false
+    t.index %w[blob_id variation_digest], name: 'index_active_storage_variant_records_uniqueness', unique: true
   end
 
-  create_table "lounge_owners", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "first_name", default: "", null: false
-    t.string "last_name", default: "", null: false
-    t.string "phone_number"
-    t.date "date_of_birth", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_lounge_owners_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_lounge_owners_on_reset_password_token", unique: true
+  create_table 'lounge_owners', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'first_name', default: '', null: false
+    t.string 'last_name', default: '', null: false
+    t.string 'phone_number'
+    t.date 'date_of_birth', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['email'], name: 'index_lounge_owners_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_lounge_owners_on_reset_password_token', unique: true
   end
 
-  create_table "lounges", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "name", null: false
-    t.string "address_street_1", null: false
-    t.string "address_street_2"
-    t.string "city", null: false
-    t.string "state", null: false
-    t.string "zip_code", null: false
-    t.string "phone_number"
-    t.string "email", null: false
-    t.text "description"
-    t.string "facebook_handle"
-    t.string "x_handle"
-    t.string "instagram_handle"
-    t.boolean "outside_cigars_allowed", default: false
-    t.boolean "outside_food_allowed", default: false
-    t.boolean "alcohol_served", default: false
-    t.boolean "outside_alcohol_allowed", default: false
-    t.boolean "food_served", default: false
-    t.string "website"
-    t.uuid "lounge_owner_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["lounge_owner_id"], name: "index_lounges_on_lounge_owner_id"
+  create_table 'lounges', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
+    t.string 'name', null: false
+    t.string 'address_street_1', null: false
+    t.string 'address_street_2'
+    t.string 'city', null: false
+    t.string 'state', null: false
+    t.string 'zip_code', null: false
+    t.string 'phone_number'
+    t.string 'email', null: false
+    t.text 'description'
+    t.string 'facebook_handle'
+    t.string 'x_handle'
+    t.string 'instagram_handle'
+    t.boolean 'outside_cigars_allowed', default: false
+    t.boolean 'outside_food_allowed', default: false
+    t.boolean 'alcohol_served', default: false
+    t.boolean 'outside_alcohol_allowed', default: false
+    t.boolean 'food_served', default: false
+    t.string 'website'
+    t.uuid 'lounge_owner_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['lounge_owner_id'], name: 'index_lounges_on_lounge_owner_id'
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "lounges", "lounge_owners"
+  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
+  add_foreign_key 'active_storage_variant_records', 'active_storage_blobs', column: 'blob_id'
+  add_foreign_key 'lounges', 'lounge_owners'
 end
