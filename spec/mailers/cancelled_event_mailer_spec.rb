cd0f5dbdcb2eab86f2818ad18a5c2dadb3730700
Odell Purifoy <opurifoy@springhealth.com>
2025-03-23 18:17:31 -0400

    build: cancelled event mailer

---
+++ spec/mailers/cancelled_event_mailer_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe CancelledEventMailer, type: :mailer do
+  include ApplicationHelper
+
+  describe 'notify' do
+    let(:member) { FactoryBot.create(:membership) }
+    let(:event) { FactoryBot.create(:event) }
+    let(:mail) { CancelledEventMailer.with(member: member, event: event).notify }
+
+    it 'renders the headers' do
+      expect(mail.subject).to eq("Event Cancelled: #{event.name}")
+      expect(mail.to).to eq([member.email])
+      expect(mail.from).to eq(['herf@gmail.com'])
+    end
+
+    it 'renders the body' do
+      expect(mail.body.encoded).to match("Important Update, #{member.first_name}!")
+      expect(mail.body.encoded).to match(event.name)
+      expect(mail.body.encoded).to match(event.date.strftime('%A, %B %d, %Y'))
+      expect(mail.body.encoded).to match(event.start_time.strftime('%I:%M %p'))
+      expect(mail.body.encoded).to match('has been cancelled')
+      expect(mail.body.encoded).to match(event.lounge.email)
+    end
+  end
+end
