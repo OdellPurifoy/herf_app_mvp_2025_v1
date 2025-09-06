d90c49d3aea2f474521cd29c1d3505a5cb3cef23
Odell Purifoy <opurifoy@springhealth.com>
2025-09-06 19:26:28 -0400

    rubocop

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -1,11 +1,15 @@
+# frozen_string_literal: true
+
 # app/controllers/admins/registrations_controller.rb
-class Admins::RegistrationsController < Devise::RegistrationsController
-  # Override if needed
+module Admins
+  class RegistrationsController < Devise::RegistrationsController
+    # Override if needed
 
-  # Only allow certain parameters
-  private
+    # Only allow certain parameters
+    private
 
-  def sign_up_params
-    params.require(:admin).permit(:email, :password, :password_confirmation)
+    def sign_up_params
+      params.require(:admin).permit(:email, :password, :password_confirmation)
+    end
   end
 end
